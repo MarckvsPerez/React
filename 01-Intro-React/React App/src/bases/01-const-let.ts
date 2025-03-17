e57c2e6df26016cfb2303adc1e21ca9e61731acd
Marc Pérez <marcantoniperez@gmail.com>
2025-03-17 23:38:37 +0100

    Actualiza la introducción y realiza cambios adicionales en el contenido.

---
+++ 01-Intro-React/React App/src/bases/01-const-let.ts
@@ -0,0 +1,14 @@
+const name = "Juan";
+const lastName = "Perez";
+
+let age = 20;
+age = 21;
+
+console.log(name, lastName, age);
+
+if (true) {
+  const name = "Eduardo";
+  console.log(name);
+}
+
+console.log(name);
