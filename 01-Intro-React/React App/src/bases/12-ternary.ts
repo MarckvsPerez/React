db9ba360893db6ca206926cc637e3a3dd540eb2b
Marc Pérez <marcantoniperez@gmail.com>
2025-03-19 16:36:19 +0100

    Agrega un nuevo archivo que muestra el uso del operador ternario y la evaluación lógica en TypeScript, incluyendo ejemplos de mensajes verificados.

---
+++ 01-Intro-React/React App/src/bases/12-ternary.ts
@@ -0,0 +1,9 @@
+const verified = true;
+
+const message = verified ? "Verified" : "Not verified";
+
+console.log(message);
+
+const second_message = verified && "Verified";
+
+console.log(second_message);
