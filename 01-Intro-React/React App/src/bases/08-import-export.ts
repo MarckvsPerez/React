7e8a6cd03a227fe490550eb1b5a639737084ea34
Marc Pérez <marcantoniperez@gmail.com>
2025-03-19 13:40:26 +0100

    Agrega nuevos archivos que definen un arreglo de héroes y sus propietarios, junto con funciones para encontrar héroes por ID y filtrar héroes por propietario en TypeScript.

---
+++ 01-Intro-React/React App/src/bases/08-import-export.ts
@@ -0,0 +1,14 @@
+import heroes, { owners } from "../data/heroes";
+
+const findHeroes = (id: number) => {
+  const hero = heroes.find((hero) => hero.id === id);
+  return hero;
+};
+
+const getHeroesByOwner = (owner: string) => {
+  return heroes.filter((hero) => hero.owner === owner);
+};
+
+console.log(findHeroes(1));
+console.log(getHeroesByOwner("DC"));
+console.log(owners);
